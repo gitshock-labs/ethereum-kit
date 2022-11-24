17f2dc9f36bef778c909a736a1f8e09fa973b5d1
avenbreaks <dev-cosmware@gitshock.com>
2022-11-24 19:08:27 +0800

    adding eth-kit

---
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/Chain.kt
@@ -0,0 +1,19 @@
+package io.horizontalsystems.ethereumkit.models
+
+enum class Chain(
+        val id: Int,
+        val coinType: Int,
+        val syncInterval: Long,
+        val isEIP1559Supported: Boolean
+) {
+    Ethereum(1, 60, 15, true),
+    BinanceSmartChain(56, 60, 15, false),
+    Polygon(137, 60, 15, true),
+    Optimism(10, 60, 15, false),
+    ArbitrumOne(42161, 60, 15, false),
+    Avalanche(43114, 60, 15, true),
+    Gitshock(212, 1, 15, false),
+    EthereumGoerli(5, 1, 15, true);
+
+    val isMainNet = coinType != 1
+}
